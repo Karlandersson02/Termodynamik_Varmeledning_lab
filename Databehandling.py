00596c72f751cb041e69282fa6398e2a8ec7e78a
KelmendMuriqi <kelmend@chalmers.se>
2024-01-24 13:36:27 +0100

    Update Databehandling.py

--- Databehandling.py
+++ Databehandling.py
@@ -36,7 +36,7 @@ class Data:
                     self.datadict['Time'].append(int(r[0])*self.mätintervall)
                     self.datadict['Amplitude'].append(float('.'.join(r[1][:-1].split(','))))
         
-        self.minindices = find_peaks(-np.array(self.a())+100, distance=int(0.9/self.get_frequency()/self.mätintervall))[0]
+        self.minindices = find_peaks(-np.array(self.a()), distance=int(0.9/self.get_frequency()/self.mätintervall))[0]
         self.maxindices = find_peaks(self.a(), distance=int(0.9/self.get_frequency()/self.mätintervall))[0]
         return
     
@@ -76,48 +76,47 @@ class Data:
             return self.minpeaks
         self.minpeaks['T']=[]
         self.minpeaks['A']=[]
-        for index in self.maxindices:
+        for index in self.minindices:
             self.minpeaks['T'].append(self.t()[index])
             self.minpeaks['A'].append(self.a()[index])
         return self.minpeaks
     
-    def get_Amplitude_mean(self):
-        amps = [self.a()[self.maxindices[i]]-self.a()[self.minindices[i]] for i in range(len())]
-        mean = sum(amps)/len(amps)
-        return mean
+    def get_Amplitude_list(self):
+        amps = np.array([self.a()[self.maxindices[i]]-self.a()[self.minindices[i]] for i in range(len())])
+        #mean = sum(amps)/len(amps)
+        return amps/2
 
 
-#class Measurement:
-#
-#    def __init__(self, data1, data2, Mätintervall, start=0, end=-1):
-#        self.termoelement1 = Data(data1, Mätintervall, start, end)
-#        self.termoelement2 = Data(data2, Mätintervall, start, end)
-#        #print(len(self.termoelement1.get_mini_peaks()[0]), len(self.termoelement2.get_mini_peaks()[0]))
-#        #print(len(self.termoelement1.get_max_peaks()), len(self.termoelement2.get_max_peaks()))
-#        self.length = 0.15
+class Measurement:
+
+    def __init__(self, data1, data2, Mätintervall, start=0, end=-1):
+        self.termoelement1 = Data(data1, Mätintervall, start, end)
+        self.termoelement2 = Data(data2, Mätintervall, start, end)
+        self.length = 0.1
 #        
 #        self.wave_info = {'Par_'+str(i): 
 #                          {'t_ij': self.termoelement2.get_max_peaks()[i][0]-self.termoelement1.get_max_peaks()[i][0],}
 #                            for i in range(len(self.termoelement2.get_max_peaks()))}
 #
-#    def get_mean(self):
-#        times = [self.wave_info[par]['t_ij'] for par in self.wave_info]
-#        return sum(times)/len(times)
-#    
-#    def get_damp(self):
-#        return self.termoelement1.get_Amplitude_mean()/self.termoelement2.get_Amplitude_mean()
-#    
-#    def get_alpha(self):
-#        return np.log(self.get_damp())/self.length
+    def get_mean(self):
+        times = [self.wave_info[par]['t_ij'] for par in self.wave_info]
+        return sum(times)/len(times)
+    
+    def get_damp(self):
+        ampdiff = self.termoelement1.get_Amplitude_mean()/self.termoelement2.get_Amplitude_mean()
+        return sum(ampdiff)/len(ampdiff)
+    
+    def get_alpha(self):
+        return np.log(self.get_damp())/self.length
     
         
 
 Temperatur1_1 = Data(Temperatur1_1_file, 5, 6000)
 Temperatur1_2 = Data(Temperatur1_2_file, 5, 6000)
-Temperatur2_1 = Data(Temperatur2_1_file, 2, 3750)
-Temperatur2_2 = Data(Temperatur2_2_file, 2, 3750)
-Temperatur3_1 = Data(Temperatur3_1_file, 2, 3000, 56000)
-Temperatur3_2 = Data(Temperatur3_2_file, 2, 3000, 56000)
+Temperatur2_1 = Data(Temperatur2_1_file, 2, 3600)
+Temperatur2_2 = Data(Temperatur2_2_file, 2, 3600)
+Temperatur3_1 = Data(Temperatur3_1_file, 2, 2750, 56000)
+Temperatur3_2 = Data(Temperatur3_2_file, 2, 2750, 56000)
 Temperaturer = [Temperatur1_1, Temperatur1_2, Temperatur2_1 ,Temperatur2_2, Temperatur3_1, Temperatur3_2]
 
 #Measurement1 = Measurement(Temperatur1_1_file, Temperatur1_2_file, 5, 6000)
@@ -126,12 +125,28 @@ Temperaturer = [Temperatur1_1, Temperatur1_2, Temperatur2_1 ,Temperatur2_2, Temp
 # print(Measurement1.get_mean())
 # print(Measurement2.get_mean())
 # print(Measurement3.get_mean())
-
-#fig, axs = plt.subplots(3, 1, figsize=(20, 10))
-#counter=0
-#colors = 200*['red', 'green', 'yellow']
+print((len(Temperatur1_1.get_max_peaks()['T']), len(Temperatur1_1.get_mini_peaks()['T'])))
+print((len(Temperatur1_2.get_max_peaks()['T']), len(Temperatur1_2.get_mini_peaks()['T'])))
+print((len(Temperatur2_2.get_max_peaks()['T']), len(Temperatur2_2.get_mini_peaks()['T'])))
+print((len(Temperatur2_1.get_max_peaks()['T']), len(Temperatur2_1.get_mini_peaks()['T'])))
+print((len(Temperatur3_1.get_max_peaks()['T']), len(Temperatur3_1.get_mini_peaks()['T'])))
+print((len(Temperatur3_2.get_max_peaks()['T']), len(Temperatur3_2.get_mini_peaks()['T'])))
+fig, axs = plt.subplots(3, 1, figsize=(20, 10))
+counter=0
+colors = 200*['red', 'green', 'yellow']
 #print(Measurement1.get_mean())
-plt.plot(Temperatur2_1.t(), Temperatur2_1.a())
-plt.scatter(Temperatur2_1.get_max_peaks()['T'], Temperatur2_1.get_max_peaks()['A'])
-plt.scatter(Temperatur2_1.get_mini_peaks()['T'], Temperatur2_1.get_mini_peaks()['A'])
+axs[0].plot(Temperatur1_1.t(), Temperatur1_1.a())
+axs[0].plot(Temperatur1_2.t(), Temperatur1_2.a())
+axs[0].scatter(Temperatur1_1.get_max_peaks()['T'], Temperatur1_1.get_max_peaks()['A'])
+axs[0].scatter(Temperatur1_2.get_max_peaks()['T'], Temperatur1_2.get_max_peaks()['A'])
+
+axs[1].plot(Temperatur2_1.t(), Temperatur2_1.a())
+axs[1].plot(Temperatur2_2.t(), Temperatur2_2.a())
+axs[1].scatter(Temperatur2_1.get_max_peaks()['T'], Temperatur2_1.get_max_peaks()['A'])
+axs[1].scatter(Temperatur2_2.get_max_peaks()['T'], Temperatur2_2.get_max_peaks()['A'])
+
+axs[2].plot(Temperatur3_1.t(), Temperatur3_1.a())
+axs[2].plot(Temperatur3_2.t(), Temperatur3_2.a())
+axs[2].scatter(Temperatur3_1.get_max_peaks()['T'], Temperatur3_1.get_max_peaks()['A'])
+axs[2].scatter(Temperatur3_2.get_max_peaks()['T'], Temperatur3_2.get_max_peaks()['A'])
 plt.show()
